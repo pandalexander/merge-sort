397943a6ef8e71e23c0aa603acfd91bcb1c6c942
Alex Seeley <alexseeley134@gmail.com>
2024-07-29 22:24:03 -0600

    Create merge function

--- merge.js
+++ merge.js
@@ -1,1 +1,30 @@
-console.log("Hello, world!");
+function merge(A, B) {
+  let newArray = [];
+
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (i < A.length && j < B.length) {
+    if (A[i] < B[j]) {
+      newArray[k++] = A[i++];
+    } else {
+      newArray[k++] = B[j++];
+    }
+  }
+
+  for (; i < A.length; i++) {
+    newArray[k++] = A[i];
+  }
+
+  for (; j < B.length; j++) {
+    newArray[k++] = B[j];
+  }
+
+  return newArray;
+}
+
+let array1 = [1, 5, 8];
+let array2 = [2, 4, 9];
+
+console.log(merge(array1, array2));
